bc6e518d04eee672ffed9047552618a7d985d20b
HuaTug <3152223434@qq.com>
2024-04-14 23:09:50 +0800

    MarkDown

--- biz/pkg/utils/transfer.go
+++ biz/pkg/utils/transfer.go
@@ -9,7 +9,7 @@ func Transfer(v interface{}) int64 {
 	if v, ok := v.(float64); ok {
 		userid = int64(v)
 	} else {
-		logrus.Info("类型断言失败，无法将变量转换为float64类型")
+		logrus.Info("类型断言失败,无法将变量转换为float64类型")
 	}
 	return userid
 }
